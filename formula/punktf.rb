1bba7c05cf71fb81bd902c0063e1a4907a0a6a37
Michael Lohr <michael@lohr.dev>
2021-08-10 14:53:30 +0200

    Update punktf.rb

--- formula/punktf.rb
+++ formula/punktf.rb
@@ -1,10 +1,7 @@
 class Punktf < Formula
   desc "A cross-platform multi-target dotfiles manager"
   homepage "https://github.com/Shemnei/punktf"
-
-  if OS.linux?
-    url "https://github.com/Shemnei/punktf/releases/download/v1.0.0-alpha/punktf-x86_64-unknown-linux-musl.tar.gz"
-  end
+  url "https://github.com/Shemnei/punktf/releases/download/v1.0.0-alpha/punktf-x86_64-unknown-linux-musl.tar.gz"
 
   conflicts_with "punktf"
 
