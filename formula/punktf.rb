f96c189ba49d112fb2238a634bf6da7ac8468115
Michael Lohr <michael@lohr.dev>
2021-08-10 14:21:45 +0200

    Create punktf.rb

---
+++ formula/punktf.rb
@@ -0,0 +1,15 @@
+class Punktf < Formula
+  version 'v1.0.0-alpha'
+  desc "A cross-platform multi-target dotfiles manager"
+  homepage "https://github.com/Shemnei/punktf"
+
+  if OS.linux?
+    url "https://github.com/Shemnei/punktf/releases/download/#{version}/punktf-x86_64-unknown-linux-gnu.tar.gz"
+  end
+
+  conflicts_with "punktf"
+
+  def install
+    bin.install "punktf"
+  end
+end
