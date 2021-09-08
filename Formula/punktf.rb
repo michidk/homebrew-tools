9a4f8b48caa97d1edf9cd74dd883dab745127c46
Michael Lohr <michael@lohr-ffb.de>
2021-09-08 19:37:10 +0200

    punktf: bump version to v1.0.1

--- Formula/punktf.rb
+++ Formula/punktf.rb
@@ -1,9 +1,9 @@
 class Punktf < Formula
   desc "A cross-platform multi-target dotfiles manager"
   homepage "https://github.com/Shemnei/punktf"
-  version "v1.0.0"
+  version "v1.0.1"
   url "https://github.com/Shemnei/punktf/releases/download/#{version}/punktf-x86_64-unknown-linux-musl.tar.gz"
-  sha256 "c4f9dc25df5a66e1bb914c9e69fac4ddb16ebb76196ce94bbce95cf8abdddc00"
+  sha256 "0da89831ba94a0bff05630c624f239b0080d13455bc7392eaa92077e546e82b4"
 
   conflicts_with "punktf"
 
