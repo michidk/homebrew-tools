6c09377af99ff9204568d63b8c8f642c88804338
Jonas Grawe <Shemnei@users.noreply.github.com>
2022-12-22 23:55:48 +0000

    punktf 2.0.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/punktf.rb
+++ Formula/punktf.rb
@@ -1,9 +1,9 @@
 class Punktf < Formula
   desc "A cross-platform multi-target dotfiles manager"
   homepage "https://github.com/Shemnei/punktf"
-  version "v1.0.1"
-  url "https://github.com/Shemnei/punktf/releases/download/#{version}/punktf-x86_64-unknown-linux-musl.tar.gz"
-  sha256 "0da89831ba94a0bff05630c624f239b0080d13455bc7392eaa92077e546e82b4"
+  version "2.0.0"
+  url "https://github.com/Shemnei/punktf/releases/download/v2.0.0/punktf-x86_64-unknown-linux-musl.tar.gz"
+  sha256 "aeb9ec2446dd1a44aeb3b7a5f1b0a8b3074393ad41dc9dae16b147b34b9ff73c"
 
   conflicts_with "punktf"
 
