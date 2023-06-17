d1c787cd4bb133356cb14e291794cca6ba2ad831
Michael Lohr <michael@lohr.dev>
2023-06-17 21:37:14 +0200

    vscli 0.1.4
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/vscli.rb
+++ Formula/vscli.rb
@@ -1,9 +1,9 @@
 class Vscli < Formula
     desc "A CLI tool to launch vscode projects, which supports devcontainers. "
     homepage "https://github.com/michidk/vscli"
-    version "0.1.3"
-    url "https://github.com/michidk/vscli/releases/download/v0.1.3/vscli-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "df8bf4034c868a55e73b31e0694f8787a636277c9eec1beee36c812c36b291e5"
+    version "0.1.4"
+    url "https://github.com/michidk/vscli/releases/download/v0.1.4/vscli-x86_64-unknown-linux-musl.tar.gz"
+    sha256 "6ce7dfbcee574c14f07245d857727b0fbfbd45c408023acb1cefd686a95bbbe7"
   
     conflicts_with "vscli"
   
