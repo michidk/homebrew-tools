e603247eda165671d8b38ed98469f5873d02c0f0
Michael Lohr <michael@lohr.dev>
2024-09-14 14:26:35 +0200

    chore: Add comments to the vscli formula which allow for automation

--- Formula/vscli.rb
+++ Formula/vscli.rb
@@ -5,21 +5,21 @@ class Vscli < Formula
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/michidk/vscli/releases/download/v#{version}/vscli-x86_64-apple-darwin.tar.gz"
-      sha256 "bb7d8e95fd2fa1aeaa07660c937840b4b55f03eeae52a4d865826a7711e2f518"
+      url "https://github.com/michidk/vscli/releases/download/v#{version}/vscli-x86_64-apple-darwin.tar.gz" # url:vscli-x86_64-apple-darwin
+      sha256 "bb7d8e95fd2fa1aeaa07660c937840b4b55f03eeae52a4d865826a7711e2f518" # sha:vscli-x86_64-apple-darwin
     elsif Hardware::CPU.arm?
-      url "https://github.com/michidk/vscli/releases/download/v#{version}/vscli-aarch64-apple-darwin.tar.gz"
-      sha256 "2bda29bda839df0097884855c63ba079f432c01e5d62968b7c65617b8150f021"
+      url "https://github.com/michidk/vscli/releases/download/v#{version}/vscli-aarch64-apple-darwin.tar.gz" # url:vscli-aarch64-apple-darwin
+      sha256 "2bda29bda839df0097884855c63ba079f432c01e5d62968b7c65617b8150f021" # sha:vscli-aarch64-apple-darwin
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/michidk/vscli/releases/download/v#{version}/vscli-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "945e6025f42f332665f5b6ab127c6ece13e3f9fb64afdc3d9b5dc8de031f913d"
+      url "https://github.com/michidk/vscli/releases/download/v#{version}/vscli-x86_64-unknown-linux-musl.tar.gz" # url:vscli-x86_64-unknown-linux-musl
+      sha256 "945e6025f42f332665f5b6ab127c6ece13e3f9fb64afdc3d9b5dc8de031f913d" # sha:vscli-x86_64-unknown-linux-musl
     elsif Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-      url "https://github.com/michidk/vscli/releases/download/v#{version}/vscli-arm-unknown-linux-gnueabihf.tar.gz"
-      sha256 "5a59c1b3a57a9d0634680143b3e004f698f845dff2f6f720ded43ee5a7c90839"
+      url "https://github.com/michidk/vscli/releases/download/v#{version}/vscli-arm-unknown-linux-gnueabihf.tar.gz" # url:vscli-arm-unknown-linux-gnueabihf
+      sha256 "5a59c1b3a57a9d0634680143b3e004f698f845dff2f6f720ded43ee5a7c90839" # sha:vscli-arm-unknown-linux-gnueabihf
     end
   end
 
