d85f54d227bc50327c166b5a3d7e05993965909b
Michael Lohr <michael@lohr.dev>
2024-09-14 12:58:40 +0200

    Update vscli formula to support macos

--- Formula/vscli.rb
+++ Formula/vscli.rb
@@ -1,14 +1,33 @@
 class Vscli < Formula
-    desc "A CLI tool to launch vscode projects, which supports devcontainers. "
-    homepage "https://github.com/michidk/vscli"
-    version "0.3.0"
-    url "https://github.com/michidk/vscli/releases/download/v0.3.0/vscli-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "945e6025f42f332665f5b6ab127c6ece13e3f9fb64afdc3d9b5dc8de031f913d"
-  
-    conflicts_with "vscli"
-  
-    def install
-      bin.install "vscli"
+  desc "A CLI/TUI that simplifies launching VSCode projects, with a focus on dev containers"
+  homepage "https://github.com/michidk/vscli"
+  version "0.3.0"
+
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/michidk/vscli/releases/download/v#{version}/vscli-x86_64-apple-darwin.tar.gz"
+      sha256 "bb7d8e95fd2fa1aeaa07660c937840b4b55f03eeae52a4d865826a7711e2f518"
+    elsif Hardware::CPU.arm?
+      url "https://github.com/michidk/vscli/releases/download/v#{version}/vscli-aarch64-apple-darwin.tar.gz"
+      sha256 "2bda29bda839df0097884855c63ba079f432c01e5d62968b7c65617b8150f021"
     end
   end
-  
+
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/michidk/vscli/releases/download/v#{version}/vscli-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "945e6025f42f332665f5b6ab127c6ece13e3f9fb64afdc3d9b5dc8de031f913d"
+    elsif Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
+      url "https://github.com/michidk/vscli/releases/download/v#{version}/vscli-arm-unknown-linux-gnueabihf.tar.gz"
+      sha256 "5a59c1b3a57a9d0634680143b3e004f698f845dff2f6f720ded43ee5a7c90839"
+    end
+  end
+
+  def install
+    bin.install "vscli"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/vscli --version")
+  end
+end
